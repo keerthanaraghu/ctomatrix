2e38935771a67c4cb9d39d6b95226722406aa443
Keerthana Rajasekaran <keerthana.raghuramank@gmail.com>
2018-10-23 02:29:32 -0700

    updated dependency

--- src/test/java/ctomatrix/FeedProcessorTest.java
+++ src/test/java/ctomatrix/FeedProcessorTest.java
@@ -33,19 +33,19 @@ public class FeedProcessorTest {
 				 "<CTO_MATRIX>\n" + 
 				 "<KMATS>\n" + 
 				 " <KMAT>\n" + 
-				 "    <ID>Z0RT</ID>\n" + 
-				 "    <MPN>MK642</MPN>\n" + 
+				 "    <ID>Z0ZT</ID>\n" + 
+				 "    <MPN>MK742</MPN>\n" + 
 				 "    <FIND_NOS>\n" + 
 				 "       <FIND_NO>\n" + 
-				 "         <ID>100</ID>\n" + 
+				 "         <ID>110</ID>\n" + 
 				 "         <Description>Base Unit</Description>\n" + 
 				 "         <Options>\n" + 
 				 "			<Option>\n" + 
-				 "                 <ID>065</ID>\n" + 
+				 "                 <ID>075</ID>\n" + 
 				 "                 <PUBLISHED_DESCRIPTION>iMac-27-inch</PUBLISHED_DESCRIPTION>\n" + 
 				 "                 <DISPLAY_ON_WEB>Y</DISPLAY_ON_WEB>\n" + 
-				 "                 <START_DATE>10/12/2018</START_DATE>\n" + 
-				 "                 <END_DATE>10/15/2018</END_DATE>\n" + 
+				 "                 <START_DATE>2018/12/25</START_DATE>\n" + 
+				 "                 <END_DATE>2018/10/15</END_DATE>\n" + 
 				 "                 <SALES_ORGS>\n" + 
 				 "                   <SALES_ORG>\n" + 
 				 "                       <ID>6458</ID>\n" + 
